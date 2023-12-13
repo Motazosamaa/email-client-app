7894f869de17e782c6491cd40fce9b491f2243ad
mohamedgaber-136 <mohamedd.gaberr.13696@gmail.com>
2023-12-13 15:29:37 +0200

    popUp Card

--- src/App.js
+++ src/App.js
@@ -1,57 +1,53 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import ContactPage from './components/EmailClient/ContactPage';
-import CalendarPage from './components/EmailClient/CalendarPage';
-import Main from './components/EmailClient/Main';
-import Navb from './items/Navbar';
-import ReportTable from './items/grid';
-
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ContactPage from "./components/EmailClient/ContactPage";
+import CalendarPage from "./components/EmailClient/CalendarPage";
+import Main from "./components/EmailClient/Main";
+import Navb from "./items/Navbar";
+import ReportTable from "./items/grid";
 
 const App = () => {
   const reportData = [
     {
-        "            ": "Today",
-        "Lead View": "0",
-        "Lead Created": "0",
-        "Email Added": "0",
-        "Contact Added": "0",
-        "Decision Date": "0",
-        "Expire Date": "0",
-        "LOA sent": "0",
-        "Signed LOA": "0"
+      "            ": "Today",
+      "Lead View": "0",
+      "Lead Created": "0",
+      "Email Added": "0",
+      "Contact Added": "0",
+      "Decision Date": "0",
+      "Expire Date": "0",
+      "LOA sent": "0",
+      "Signed LOA": "0",
     },
     {
-        "            ": "Current Week",
-        "Lead View": "0",
-        "Lead Created": "0",
-        "Email Added": "0",
-        "Contact Added": "0",
-        "Decision Date": "0",
-        "Expire Date": "0",
-        "LOA sent": "0",
-        "Signed LOA": "0"
+      "            ": "Current Week",
+      "Lead View": "0",
+      "Lead Created": "0",
+      "Email Added": "0",
+      "Contact Added": "0",
+      "Decision Date": "0",
+      "Expire Date": "0",
+      "LOA sent": "0",
+      "Signed LOA": "0",
     },
     {
-        "            ": "Current Month",
-        "Lead View": "0",
-        "Lead Created": "0",
-        "Email Added": "0",
-        "Contact Added": "0",
-        "Decision Date": "0",
-        "Expire Date": "0",
-        "LOA sent": "0",
-        "Signed LOA": "0"
+      "            ": "Current Month",
+      "Lead View": "0",
+      "Lead Created": "0",
+      "Email Added": "0",
+      "Contact Added": "0",
+      "Decision Date": "0",
+      "Expire Date": "0",
+      "LOA sent": "0",
+      "Signed LOA": "0",
     },
     // ... more data
-];
+  ];
   return (
     <Router>
-
       <div>
-        
-        <Navb/>
-        <br/>
+        <Navb />
+        <br />
         <Routes>
           <Route path="/contacts" element={<ContactPage />} />
           <Route path="/calendar" element={<CalendarPage />} />
--- src/components/EmailClient/ContactPage.jsx
+++ src/components/EmailClient/ContactPage.jsx
@@ -2,20 +2,34 @@ import React, { useEffect, useState } from "react";
 import ContactCard from "../CRMSystem/Contactcard";
 import { useLocation } from "react-router-dom";
 import exampleCompanies from "../../data/exampleCompanies";
+import Modal from "react-bootstrap/Modal";
+import { useNavigate } from "react-router-dom";
 const ContactPage = () => {
+  const [show, setShow] = useState(false);
+  const navigator = useNavigate();
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   let { state } = useLocation();
   let [data, SetNewData] = useState({});
   let setFormData = (e) => {
     e.preventDefault();
-    let newUser = {
-      name: e.target[0].value,
-      email: e.target[1].value,
-      phone: e.target[2].value,
-      address: e.target[3].value,
-      notes: "Some notes about Eva.",
-    };
-
-    SetNewData({ ...data, ...newUser });
+    if (
+      e.target[0].value == "" ||
+      e.target[1].value == "" ||
+      e.target[2].value == "" ||
+      e.target[3].value == ""
+    ) {
+    } else {
+      navigator("/");
+      let newUser = {
+        name: e.target[0].value,
+        email: e.target[1].value,
+        phone: e.target[2].value,
+        address: e.target[3].value,
+        notes: "Some notes about Eva.",
+      };
+      SetNewData({ ...data, ...newUser });
+    }
   };
   useEffect(() => {
     if (data.name != undefined) {
@@ -26,66 +40,92 @@ const ContactPage = () => {
       console.log(exampleCompanies, "table");
     }
   }, [data]);
+  useEffect(() => {
+    handleShow();
+  }, []);
+
   return (
-    <div className="d-flex  justify-content-center align-items-center ">
-      <form
-        class="form border mx-5"
-        onSubmit={(e) => {
-          setFormData(e);
+    <Modal
+      show={show}
+      onHide={handleClose}
+      className="modalParent"
+      backdrop="static"
+      keyboard={false}
+    >
+      <Modal.Header
+        closeButton
+        onClick={() => {
+          navigator("/");
         }}
       >
-        <p class="title">company information </p>
-        <div class="flex">
-          <label>
-            <input type="text" class="input" />
-            <span className="text-primary">company</span>
-          </label>
+        <Modal.Title>
+          {" "}
+          <p class="title">company information </p>
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        {" "}
+        <div className="d-flex flex-column flex-lg-row   justify-content-center align-items-center ">
+          {" "}
+          <form
+            class="form border m-5"
+            onSubmit={(e) => {
+              setFormData(e);
+            }}
+          >
+            <div class="flex">
+              <label>
+                <input type="text" class="input" />
+                <span className="text-primary">company</span>
+              </label>
 
-          <label>
-            <input type="text" class="input" />
-            <span className="text-primary">company Phone</span>
-          </label>
-        </div>
-        <div className="flex">
-          <label>
-            <input type="text" class="input" />
-            <span className="text-primary">company city</span>
-          </label>
+              <label>
+                <input type="text" class="input" />
+                <span className="text-primary">company Phone</span>
+              </label>
+            </div>
+            <div className="flex">
+              <label>
+                <input type="text" class="input" />
+                <span className="text-primary">company city</span>
+              </label>
 
-          <label>
-            <input type="text" class="input" />
-            <span className="text-primary">company zip</span>
-          </label>
-        </div>
+              <label>
+                <input type="text" class="input" />
+                <span className="text-primary">company zip</span>
+              </label>
+            </div>
 
-        <div className="flex">
-          <label>
-            <input required="" placeholder="" type="text" class="input" />
-            <span className="text-primary"> Company DUN#</span>
-          </label>
-          <label>
-            <input required="" placeholder="" type="text" class="input" />
-            <span className="text-primary"> Revenue</span>
-          </label>
+            <div className="flex">
+              <label>
+                <input required="" placeholder="" type="text" class="input" />
+                <span className="text-primary"> Company DUN#</span>
+              </label>
+              <label>
+                <input required="" placeholder="" type="text" class="input" />
+                <span className="text-primary"> Revenue</span>
+              </label>
+            </div>
+            <div className="flex">
+              <div>
+                <label htmlFor="">companyState:</label>
+                <select name="" id="">
+                  <option value="texas">texas</option>
+                </select>{" "}
+              </div>
+              <div>
+                <label htmlFor="">LeadPool:</label>
+                <select name="" id="">
+                  <option value="texas">texas</option>
+                </select>{" "}
+              </div>
+            </div>
+            <input type="submit" class="submit" value={"save"} />
+          </form>
+          {state == null ? " " : <ContactCard contact={state.contacts[0]} />}
         </div>
-        <div className="flex">
-          <div>
-            <label htmlFor="">companyState:</label>
-            <select name="" id="">
-              <option value="texas">texas</option>
-            </select>{" "}
-          </div>
-          <div>
-            <label htmlFor="">LeadPool:</label>
-            <select name="" id="">
-              <option value="texas">texas</option>
-            </select>{" "}
-          </div>
-        </div>
-        <input type="submit" class="submit" value={"save"} />
-      </form>
-      {state == null ? " " : <ContactCard contact={state.contacts[0]} />}
-    </div>
+      </Modal.Body>
+    </Modal>
   );
 };
 
